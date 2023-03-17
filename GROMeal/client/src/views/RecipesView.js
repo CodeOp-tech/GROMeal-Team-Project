c98cb466d81780ea0599cb58f3a080746c719a35
name <natalia26.ag@gmail.com>
2023-03-17 02:37:26 +0100

    all working

--- GROMeal/client/src/views/RecipesView.js
+++ GROMeal/client/src/views/RecipesView.js
@@ -20,9 +20,8 @@ function RecipesView(props){
     const [featVisible, setfeatVisible] = useState(true);
     const [recipes, setRecipes] = useState([]);
     const [featRecipe, setFeatRecipe] = useState([]);
-    let [userRecipe, setUserRecipe] = useState([]);
+    // let [userRecipe, setUserRecipe] = useState([]);  //FECTH PUT & GET
     const [ addedRecipe, setAddedRecipe ] = useState(EMPTY_FORM);
-    const [reloadRecipe, setReloadRecipes] = useState([]);
 
     useEffect(() => {
         getRandomRecipes();
@@ -40,25 +39,50 @@ function RecipesView(props){
 
     }
 
-    //FETCH GET NEW RECIPE FROM USER
-    function getUserRecipe() { 
-        fetch(`/1`)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error(
-                        `Server error: ${response.status}: ${response.statusText}`
-                    );
-                }
-            })
-            .then(data => {
-                setUserRecipe(data);
-            })
-            .catch (error =>  {
-                console.log(`Error: ${error}`);
-            });
-    }
+//  //FETCH GET NEW RECIPE FROM USER
+//     function getUserRecipe() { 
+//         fetch(`/1`)
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.json();
+//                 } else {
+//                     throw new Error(
+//                         `Server error: ${response.status}: ${response.statusText}`
+//                     );
+//                 }
+//             })
+//             .then(data => {
+//                 setUserRecipe(data);
+//             })
+//             .catch (error =>  {
+//                 console.log(`Error: ${error}`);
+//             });
+//     }
+
+//  //FETCH PUT TO UPDATE RECIPE FROM USER
+//     const updateRecipe = async id => {
+//     let recipe = userRecipe.find(r => r.id === id);
+//     recipe.complete = !recipe.complete;
+//     console.log(id)
+//     let options = {
+//         method: "PUT",
+//         headers: { "Content-Type": "application/json"},
+//         body: JSON.stringify(recipe)
+//     };
+    
+//     try { 
+//         let response = await fetch(`/:planId`, options);
+//         if (response.ok) {
+//             getUserRecipe();
+//         } else {
+//             console.log(`Server error: ${response.status}:
+//             ${response.statusText}`);
+//         }
+//     } catch (err) {
+//         console.log(`Network error: ${err.message}`);
+//     }
+//     };
+
     
     //WORKING
     //FETCH POST NEW RECIPE FROM USER
@@ -80,35 +104,12 @@ function RecipesView(props){
         console.log(addedRecipe)
       };
 
-    //FETCH PUT TO UPDATE RECIPE FROM USER
-    const updateRecipe = async id => {
-    let recipe = userRecipe.find(r => r.id === id);
-    recipe.complete = !recipe.complete;
-    console.log(id)
-    let options = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify(recipe)
-    };
-    
-    try { 
-        let response = await fetch(`/:planId`, options);
-        if (response.ok) {
-            getUserRecipe();
-        } else {
-            console.log(`Server error: ${response.status}:
-            ${response.statusText}`);
-        }
-    } catch (err) {
-        console.log(`Network error: ${err.message}`);
-    }
-    };
+   
     //WORKING 
     //FUNCTION TO CLICK ON RECIPE, VISUALIZE RECIPE ON TOP & ADDS RECIPE'S DATA TO CONST addedRecipe
     function showFeatRecipe(id){
         let selectedRecipe = recipes.find(r => r.id === id);
         setFeatRecipe(selectedRecipe);
-        setReloadRecipes(selectedRecipe)
         // const [reloadRecipe, setReloadRecipes] = useState([]);
         console.log(selectedRecipe);
         // console.log(selectedRecipe.title);
@@ -123,79 +124,84 @@ function RecipesView(props){
         setfeatVisible(featVisible);
       };
 
+    //FORM INPUT
+    const handleChange = event => {
+    // console.log(event.target.id)
+
+    let  value  = event.target.value;
+    let name = event.target.name;
+    
+    setAddedRecipe((addedRecipe) => ({...addedRecipe, [name]: value}));
+    };
+
     const handleSubmit = event => {
         
         event.preventDefault();
         addRecipe(addedRecipe);
-        let API_id = featRecipe.id;
-        let recipe_title = featRecipe.title;
-        let recipe_image = featRecipe.image;
-        let meal_type = addedRecipe.meal_type;
-        let week_day = addedRecipe.week_day;
-        let servings = addedRecipe.servings;
-        setAddedRecipe(EMPTY_FORM); //que se ponga en blanco solo lo que cambia el usuario en handlechange
-        setAddedRecipe((addedRecipe) => ({...addedRecipe, API_id: API_id, recipe_title: recipe_title, recipe_image: recipe_image, meal_type: meal_type, week_day: week_day, servings: servings}));
-    
+        setAddedRecipe((addedRecipe) => ({...addedRecipe, meal_type: "", week_day: "", servings: 1}));
+
     };
     
 
-    const handleChange = event => {
-        // console.log(event.target.id)
-
-        let  value  = event.target.value;
-        let name = event.target.name;
-        
-        setAddedRecipe((addedRecipe) => ({...addedRecipe, [name]: value}));
-    };
+    
    
     let weekDayArray = ['monday', 'tuesday', 'wednesday', 'thursday', "friday", "saturday", "sunday"];
     let mealType = ['breakfast', "lunch", "dinner"];
 
     return (
+
         <div className="RecipesView">
-            <h1>Select your favorite meals</h1>
+            <h1 className='favoriteTitle'>Select your favorite meals</h1>
+            
             <div id={featRecipe.id} className= { featVisible ? "invisible" : 'visible' }> 
                 <div className="featBlock">
                     <img src={featRecipe.image} alt="recipe" className="featImage"></img>
-                    <div>
-                        <h3 className="featLegend">{featRecipe.title}</h3>
-                        <h5 className="featLegend">Ready in: {featRecipe.readyInMinutes} min</h5>
+                    <div className="featLegend">
+                        <h3 className="featLegendText">{featRecipe.title}</h3>
+                        <h4 className="featLegendText">Ready in: {featRecipe.readyInMinutes} min</h4>
                         <h5 className="featLegend">{featRecipe.instructions}</h5>
-                        <h5>I want to eat this meal on :</h5>
-                        <div>
-                            <form onSubmit = {handleSubmit}>
-                                <select className = "dropdown" name='week_day' id="selected"
-                                    onChange = { handleChange }
-                                    >
-                                    <option selected id="editOptions"> Select a day </option> 
-                                    { weekDayArray.map(recipe => (
-                                        <option id="editOptions" value={recipe}>{recipe}</option>
-                                    )) }
+                        <h5 className="featLegend">I want to eat this meal on :</h5>
+                        <div className="featBlockform">
+                            <form className="featLegendform" onSubmit = {handleSubmit}>
+                                <label className="featLegendform">
+                                    Select a day
+                                    <select required className = "mealInput" name='week_day' id="selected" value={addedRecipe.week_day}
+                                        onChange = { handleChange }
+                                        >
+                                        <option selected id="editOptions" value={""}></option> 
+                                        { weekDayArray.map(day => (
+                                            <option id="editOptions" value={day}>{day}</option>
+                                        )) }
 
-                                </select>
-                                <select className = "dropdown" name='meal_type' id="selected"
-                                    onChange = { handleChange }
-                                    >
-                                    <option selected id="editOptions"> Select a meal </option> 
-                                    { mealType.map(recipe => (
-                                        <option id="editOptions" value={recipe}>{recipe}</option>
-                                    )) }
+                                    </select>
+                                </label>
+                                <label className="featLegendform">
+                                    Select a meal
+                                    <select required className = "mealInput" name='meal_type' id="selected" value={addedRecipe.meal_type}
+                                        onChange = { handleChange }
+                                        >
+                                        <option selected id="editOptions" value={""}></option> 
+                                        { mealType.map(meal => (
+                                            <option id="editOptions" value={meal}>{meal}</option>
+                                        )) }
 
-                                </select>
-                                <label>
+                                    </select>
+                                </label>
+                                <label className="featLegendform">
                                     Serving
-                                    <input type="number" id="serving" name="servings" value={addedRecipe.servings}
+                                    <input className = "mealInput" type="number" id="serving" name="servings" value={addedRecipe.servings}
                                     min="1"
                                     onChange = { handleChange }
                                     ></input>
                                 </label>
-                                
-                                <button >
-                                    add recipe
-                                </button>
+                                <label className="buttonFeatLegendform">
+                                    <button className="buttonFeatLegendform">
+                                        add recipe
+                                    </button>
+
+                                </label>
 
                             </form>
-                        <ul value={featRecipe.title}></ul>
                         </div>
 
                     </div>
@@ -203,18 +209,26 @@ function RecipesView(props){
                 </div>
                             
             </div>
-            <button>
-                <NavLink to="/">
-                    Home 
-                </NavLink>
-            </button>
+
+            <div className='NavSection-RecipesView'>
+                <button className='NavButton-RecipesView'>
+                    <NavLink className='NavLink-RecipesView' to="/">
+                        Home 
+                    </NavLink>
+                </button>
+                <button className='NavButton-RecipesView'>
+                    <NavLink className='NavLink-RecipesView'to={`/weekPlan/1`}>
+                        weekplan 
+                    </NavLink>
+                </button>
+            </div>
             <div className="recipesGrid" >
                 {
                 recipes.map(recipe => (
                     <div  onClick={() => handleChangeView(false)}>
                         <div className="recipeBlock" id={recipe.id} key={recipe.id} onClick={() => showFeatRecipe(recipe.id)}>
                             <img src={recipe.image} alt="recipe"></img>
-                            <h5 className="imageLeg">{recipe.title}</h5>
+                            <h5 className="imageLeg" id='recipeTitle'>{recipe.title}</h5>
                             <h6 className="imageLeg">Ready in: {recipe.readyInMinutes} min</h6>
                             
                             {/* <Link to="/page2">
