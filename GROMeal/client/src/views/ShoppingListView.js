64bd599495df76351d422f1a7f8de3fefcd873a8
immatte <matte_isabelle@yahoo.fr>
2023-03-22 18:01:12 +0100

    UPDATE PDF DOWNLOAD

--- GROMeal/client/src/views/ShoppingListView.js
+++ GROMeal/client/src/views/ShoppingListView.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink, useParams, Route, Routes, useNavigate } from 'react-router-dom';
 import SpoonApi from "../helpers/SpoonApi";
+import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+// const doc = new jsPDF();
+
 
 const API_KEY = "5e24157871f640c684861272be1907c9";
 const ISA_KEY = "0fe1d39b2dbf4cb5a8737ca807512217";
@@ -11,36 +16,38 @@ const ANAMARI_KEY4 = "2249a308392d4fefb388c16c01db781a";
 
 function ShoppingListView() {
 
-    const [recipes, setRecipes] = useState([]);
-    const [planRecipes, setPlanRecipes] = useState([]);
-    const [ingredients, setIngredients] = useState([]);
-    const [list, setList] = useState([]);
-    const { planId } = useParams();
+  const [recipes, setRecipes] = useState([]);
+  const [planRecipes, setPlanRecipes] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [list, setList] = useState([]);
+  // const [pdf, setPdf] = useState(null);
+  const { planId } = useParams();
+
+  // useEffect(() => {
+  //     getRandomRecipes();
+  // }, []);
+
+  // async function getRandomRecipes() {
+  //     let uresponse = await SpoonApi.getRandomRecipes();
+  //     console.log(uresponse);
+  //     if (uresponse.ok) {
+  //         setRecipes(uresponse.data.recipes);
+          
+  //     } else {
+  //         console.log('Error:', uresponse.error);
+  //     }
+
+  // }
+
+  useEffect(() => {
+    getPlanRecipes();
+  }, []);
+
+  useEffect(() => {
+      getIngredients();
+    }, [planRecipes]);
   
-    // useEffect(() => {
-    //     getRandomRecipes();
-    // }, []);
 
-    // async function getRandomRecipes() {
-    //     let uresponse = await SpoonApi.getRandomRecipes();
-    //     console.log(uresponse);
-    //     if (uresponse.ok) {
-    //         setRecipes(uresponse.data.recipes);
-            
-    //     } else {
-    //         console.log('Error:', uresponse.error);
-    //     }
-
-    // }
-
-    useEffect(() => {
-      getPlanRecipes();
-    }, []);
-
-    useEffect(() => {
-        getIngredients();
-      }, [planRecipes]);
-  
   // Get All recipes from a plan
   async function getPlanRecipes() {
   
@@ -96,54 +103,56 @@ console.log(planRecipes);
 
 
 
-        const shoppingList = [{id: 1, item_name: 'canned corn', amount: 425.243, unit: 'g'},
-        {id: 2, item_name: 'canned black beans', amount: 425.243, unit: 'g'},
-        {id: 3, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
-        {id: 4, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'},
-        {id: 5, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'},
-        {id: 6, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'},
-        {id: 7, item_name: 'milk', amount: 366, unit: 'ml'},
-        {id: 8, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
-        {id: 9, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
-        {id: 10, item_name: 'canned corn', amount: 425.243, unit: 'g'}, 
-        {id: 11, item_name: 'canned black beans', amount: 425.243, unit: 'g'}, 
-        {id: 12, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
-        {id: 13, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'}, 
-        {id: 14, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'}, 
-        {id: 15, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'}, 
-        {id: 16, item_name: 'milk', amount: 366, unit: 'ml'}, 
-        {id: 17, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
-        {id: 18, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
-        {id: 19, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
-        {id: 20, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
-        {id: 21, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
-        {id: 22, item_name: 'eggs', amount: 2, unit: ''}, 
-        {id: 23, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
-        {id: 24, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
-        {id: 25, item_name: 'oreo cookies', amount: 12, unit: ''}, 
-        {id: 26, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
-        {id: 27, item_name: 'salt', amount: 1, unit: 'tsp'}, 
-        {id: 28, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
-        {id: 29, item_name: 'sour cream', amount: 57.5, unit: 'ml'},
-        {id: 30, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
-        {id: 31, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
-        {id: 32, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
-        {id: 33, item_name: 'water', amount: 0.75, unit: 'cups'}, 
-        {id: 34, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
-        {id: 35, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
-        {id: 36, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
-        {id: 37, item_name: 'eggs', amount: 2, unit: ''}, 
-        {id: 38, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
-        {id: 39, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
-        {id: 40, item_name: 'oreo cookies', amount: 12, unit: ''}, 
-        {id: 41, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
-        {id: 42, item_name: 'salt', amount: 1, unit: 'tsp'}, 
-        {id: 43, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
-        {id: 44, item_name: 'sour cream', amount: 57.5, unit: 'ml'}, 
-        {id: 45, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
-        {id: 46, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
-        {id: 47, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
-        {id: 48, item_name: 'water', amount: 0.75, unit: 'cups'}]
+      const shoppingList = [
+            {id: 1, item_name: 'canned corn', amount: 425.243, unit: 'g'},
+            {id: 2, item_name: 'canned black beans', amount: 425.243, unit: 'g'},
+            {id: 3, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
+            {id: 4, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'},
+            {id: 5, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'},
+            {id: 6, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'},
+            {id: 7, item_name: 'milk', amount: 366, unit: 'ml'},
+            {id: 8, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
+            {id: 9, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
+            {id: 10, item_name: 'canned corn', amount: 425.243, unit: 'g'}, 
+            {id: 11, item_name: 'canned black beans', amount: 425.243, unit: 'g'}, 
+            {id: 12, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
+            {id: 13, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'}, 
+            {id: 14, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'}, 
+            {id: 15, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'}, 
+            {id: 16, item_name: 'milk', amount: 366, unit: 'ml'}, 
+            {id: 17, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
+            {id: 18, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
+            {id: 19, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
+            {id: 20, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
+            {id: 21, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
+            {id: 22, item_name: 'eggs', amount: 2, unit: ''}, 
+            {id: 23, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
+            {id: 24, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
+            {id: 25, item_name: 'oreo cookies', amount: 12, unit: ''}, 
+            {id: 26, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
+            {id: 27, item_name: 'salt', amount: 1, unit: 'tsp'}, 
+            {id: 28, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
+            {id: 29, item_name: 'sour cream', amount: 57.5, unit: 'ml'},
+            {id: 30, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
+            {id: 31, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
+            {id: 32, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
+            {id: 33, item_name: 'water', amount: 0.75, unit: 'cups'}, 
+            {id: 34, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
+            {id: 35, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
+            {id: 36, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
+            {id: 37, item_name: 'eggs', amount: 2, unit: ''}, 
+            {id: 38, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
+            {id: 39, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
+            {id: 40, item_name: 'oreo cookies', amount: 12, unit: ''}, 
+            {id: 41, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
+            {id: 42, item_name: 'salt', amount: 1, unit: 'tsp'}, 
+            {id: 43, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
+            {id: 44, item_name: 'sour cream', amount: 57.5, unit: 'ml'}, 
+            {id: 45, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
+            {id: 46, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
+            {id: 47, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
+            {id: 48, item_name: 'water', amount: 0.75, unit: 'cups'}
+              ]
 
         //NEWLIST sum amounts when names match
         
@@ -203,7 +212,24 @@ async function deleteItem( id) {
         console.log(`Server error: ${err.message}`);
     }
   }
-          
+         
+  // DOWNLOAD FUNCTION
+  let weekDayArray = ['monday', 'tuesday', 'wednesday', 'thursday', "friday", "saturday", "sunday"];
+
+  const downloadPdf = event =>{
+    var doc = new jsPDF({
+      // unit:"mm"
+    });
+    shoppingList.forEach(function(ingredient,i){
+      doc.text(50,10+i*10, ingredient.item_name + " " + ingredient.amount + " " + ingredient.unit +"\n" );
+    });
+    doc.setFontSize(5);
+    doc.save("HelloWorld.pdf");
+  }
+  
+  
+    
+
         
     return (
     <div className="container">
@@ -220,6 +246,9 @@ async function deleteItem( id) {
                   </NavLink>
               </button>
             </div>
+        </div>
+        <div>
+          <button onClick={downloadPdf}>Download my Shopping list</button>
         </div>
         <div>
             <h1 style={{marginBottom: "100px"}}>My Shopping List</h1>
